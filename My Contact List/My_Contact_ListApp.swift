e2655a3a6b163c086103ab4cc210f8ec04cc8d4d
Duy Huynh <117799318+duyhnynh@users.noreply.github.com>
2024-08-01 23:43:59 +0700

    Initial Commit

---
+++ My Contact List/My_Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Contact_ListApp.swift
+//  My Contact List
+//
+//  Created by Duy Huynh on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct My_Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
